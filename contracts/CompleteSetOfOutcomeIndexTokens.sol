78845da874bdbe3cb581900bc321991397748b81
Jasper De gooijer <jasperdegooijer@jaspers-mbp.localdomain>
2019-06-21 13:17:53 +0200

    rewrote the contracts

--- contracts/CompleteSetOfOutcomeIndexTokens.sol
+++ contracts/CompleteSetOfOutcomeIndexTokens.sol
@@ -9,7 +9,7 @@ import "./../libraries/augur/source/contracts/trading/IShareToken.sol";
 import "./../libraries/augur/source/contracts/reporting/IMarket.sol";
 import "./../libraries/augur/source/contracts/trading/CompleteSets.sol";
 import "./../libraries/augur/source/contracts/trading/ICash.sol";
-
+import "./../libraries/augur/source/contracts/trading/ClaimTradingProceeds.sol";
 
 /* 
 	TODO: 
@@ -21,6 +21,7 @@ contract CompleteSetOfOutcomeIndexTokens is MintableERC20Token {
 
 	CompleteSets constant private completeSets = CompleteSets(0x48FCc9d538B9C86bA9D35b3eB0e7f64EE2B4664f);
 	ICash constant private cash = ICash(0x2Da4d465978981BD75BbaC4C9f3bdA10bE0B465c);
+	ClaimTradingProceeds constant private claimTradingProceeds = ClaimTradingProceeds(0x9e94fdea4aace8c61eeb1dc2d3c55bfc7b7e8739);
 	address constant private augur = 0x990B2D2aF7e87cd015A607c3A95d7622c9bBeDe1;
 
 	mapping(address => uint256) marketsToWeight;
@@ -32,21 +33,22 @@ contract CompleteSetOfOutcomeIndexTokens is MintableERC20Token {
 
 	constructor (
 		IMarket[] _markets,
-		uint256[] _weights
+		uint256[] _weights,
+		uint256 _numOutcomes
 	)  
 	public 
 	{
 		// For each market there needs to be a decision on how weight it carries
 		require(_markets.length == _weights.length);
 
-		for (uint256 outcome = 0; outcome < _indexes.length; outcome++) {
+		for (uint256 outcome = 0; outcome < _numOutcomes; outcome++) {
 			// Create ERC20 token representing an index of outcomes
 			outcomeIndexTokens.push(new OutcomeIndexToken());
 
 			// Create an array with all ShareTokens
-			IShareToken[] index;
-			for (uint256 x = 0; x < _markets.getNumberOfOutcomes().length; x++) {
-				index.push(address(_market.getOutcomeToken(outcome)));
+			IShareToken[] storage index;
+			for (uint256 x = 0; x < _markets.length; x++) {
+				index.push(_markets[x].getShareToken(outcome));
 			}
 			// Push the index array to an array containing all indexes
 			indexes.push(index);
@@ -73,6 +75,7 @@ contract CompleteSetOfOutcomeIndexTokens is MintableERC20Token {
 		for (uint256 i = 0; i < markets.length; i++) {
 			// Calculate the amount of shares that have to be bought 
 			uint256 weightedAmount = msg.value.mul(marketsToWeight[markets[i]]).div(100);
+			require(weightedAmount >= markets[i].getNumTicks());
 			completeSets.publicBuyCompleteSets.value(weightedAmount)(markets[i], weightedAmount);
 		}
 		// For each existing index token mint the amount in eth for the sender
@@ -83,7 +86,7 @@ contract CompleteSetOfOutcomeIndexTokens is MintableERC20Token {
 
 	// Returns the contract's ShareToken balance of a certain market
 	function getMarketBalance(
-		bool _outcome,
+		uint256 _outcome,
 		IMarket _market
 	) 
 	public 
@@ -99,7 +102,7 @@ contract CompleteSetOfOutcomeIndexTokens is MintableERC20Token {
 	view 
 	returns (bool resolved) 
 	{
-		for (uint i = 0; i < index.length; i++) {
+		for (uint i = 0; i < markets.length; i++) {
 			if (markets[i].isFinalized() == false) {
 				return false;
 			}
@@ -116,10 +119,7 @@ contract CompleteSetOfOutcomeIndexTokens is MintableERC20Token {
 		require(!marketsFinalized);
 
 		for (uint x = 0; x < markets.length; x++) {
-			if (outcome == markets[x].outcome()) {
-				winningsByWeight = winningsByWeight.add(weights[x]);
-				markets[x].claim(outcome);	
-			}
+			claimTradingProceeds.claimTradingProceeds(markets[x], address(this));
 		}
 
 		marketsFinalized = true;
